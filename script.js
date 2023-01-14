598dc3adbe166ddc4ce547e1f94a5a5ea95aba8f
Juliana <jumiranda0501@gmail.com>
2023-01-14 09:16:48 -0300

    Handle book form submition

--- script.js
+++ script.js
@@ -1,42 +1,67 @@
 let myLibrary = [];
 
-addBookToLibrary();
-displayData();
 
-function Book(author, title, pages, isRead) {
+// Page elements
+const table = document.getElementById("data");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const finishedInput = document.getElementById("finished");
+const formSubmitButton = document.getElementById("submit");
+
+
+// Buttons listeners
+formSubmitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    addBookToTable();
+    clearFormInputs();
+});
+
+
+function Book(author, title, pages, isFinished) {
     this.author = author;
     this.title = title;
     this.pages = pages;
-    this.isRead = isRead;
+    this.isFinished = isFinished;
 }
+
 
 function addBookToLibrary() {
-    myLibrary.push(new Book("Author One", "Title 1", 249, true));
-    myLibrary.push(new Book("Author Two", "Title 2", 120, true));
-    myLibrary.push(new Book("Author Three", "Title 3", 532, false));
+
+    let author = authorInput.value;
+    let title = titleInput.value;
+    let pages = pagesInput.value;
+    let isFinished = finishedInput.checked;
+
+    myLibrary.push(new Book(author, title, pages, isFinished));
+
 }
 
-function displayData() {
 
-    const table = document.getElementById("data");
+function addBookToTable() {
 
-    // Include books on html table rows
-    for (let i = 0; i < myLibrary.length; i++) {
+    // Create an empty <tr> element and add it to the last position of the table:
+    const row = table.insertRow(-1);
 
-        // Create an empty <tr> element and add it to the last position of the table:
-        const row = table.insertRow(-1);
+    // Insert cells:
+    const authorCell = row.insertCell(0);
+    const titleCell = row.insertCell(1);
+    const pagesCell = row.insertCell(2);
+    const checkboxCell = row.insertCell(3);
 
-        // Insert cells:
-        var authorCell = row.insertCell(0);
-        var titleCell = row.insertCell(1);
-        var pagesCell = row.insertCell(2);
-        var checkboxCell = row.insertCell(3);
+    // Add content to the new cells:
+    authorCell.innerHTML = myLibrary[myLibrary.length -1].author;
+    titleCell.innerHTML = myLibrary[myLibrary.length -1].title;
+    pagesCell.innerHTML = myLibrary[myLibrary.length -1].pages;
+    checkboxCell.innerHTML = myLibrary[myLibrary.length -1].isFinished;
 
-        // Add content to the new cells:
-        authorCell.innerHTML = myLibrary[i].author;
-        titleCell.innerHTML = myLibrary[i].title;
-        pagesCell.innerHTML = myLibrary[i].pages;
-        checkboxCell.innerHTML = myLibrary[i].isRead;
-        
-    }
+}
+
+
+function clearFormInputs() {
+    authorInput.value = "";
+    titleInput.value = "";
+    pagesInput.value = "";
+    finishedInput.checked = false;
 }
